d1c887c8b8bf46582afe273306a2c4b3de6e3542
p738dev <pmech14@interia.pl>
2023-05-26 10:34:13 +0200

    RT-5: Edit AddUser components, add states

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
   StyledAddContainer,
@@ -9,27 +9,48 @@ import {
 } from "./AddUser.css";
 
 const AddUser = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [country, setCountry] = useState("");
+
+  const savePerson = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <StyledAddContainer>
       <StyledHeaderInformation>Dodaj użytkownika</StyledHeaderInformation>
-      <StyledForm>
+      <StyledForm onSubmit={savePerson}>
         <StyledInput
           type="text"
           placeholder="Wprowadź imię"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
         <StyledInput
           type="text"
           placeholder="Wprowadź email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <StyledInput
           type="number"
           placeholder="Wprowadź numer telefonu"
+          name="phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
         />
         <StyledInput
           type="text"
           placeholder="Kraj"
+          name="country"
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
         />
-        <StyledButton>Dodaj</StyledButton>
+        <StyledButton type="submit">Dodaj</StyledButton>
       </StyledForm>
     </StyledAddContainer>
   );
